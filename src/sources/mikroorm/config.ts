9426b6e245472dcf79633aa6b3dd454a6c06ff13
Daniil Bogaciov <Daniil.Bogaciov@endava.com>
2025-06-03 14:22:41 +0300

    check6

--- src/sources/mikroorm/config.ts
+++ src/sources/mikroorm/config.ts
@@ -5,24 +5,27 @@ import { Owner, Car, Seller } from './models/index.js';
 const driverOptions: Options['driverOptions'] = {};
 
 if (process.env.NODE_ENV === 'production') {
-  driverOptions.connection = { ssl: true };
+driverOptions.connection = {ssl: {
+  require: true,
+  rejectUnauthorized: false
+} };
 }
 
 const config: Options = {
-  entities: [Owner, Car, Seller],
-  type: 'postgresql' as const,
-  clientUrl: process.env.POSTGRES_DATABASE_URL,
-  migrations: {
-    path: 'src/sources/mikroorm/migrations',
-    emit: 'ts',
-    disableForeignKeys: false,
-  },
-  driverOptions,
-  debug: process.env.DATABASE_LOGGING === 'true',
+entities: [Owner, Car, Seller],
+type: 'postgresql' as const,
+clientUrl: process.env.POSTGRES_DATABASE_URL,
+migrations: {
+  path: 'src/sources/mikroorm/migrations',
+  emit: 'ts',
+  disableForeignKeys: false,
+},
+driverOptions,
+debug: process.env.DATABASE_LOGGING === 'true',
 };
 
 export const init = async () => {
-  orm = await MikroORM.init(config);
+orm = await MikroORM.init(config);
 };
 
 export default config;
